579256e182cf5db5eba3a4397f14e955d9c88792
Pol Gubau <gubaupol@gmail.com>
2022-05-15 13:07:55 +0200

    husky, prettier conf, eslint configuration

--- firebase/client.js
+++ firebase/client.js
@@ -1,18 +1,17 @@
-import * as firebase from 'firebase'
+import * as firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC70LAhPV1qHunyNoZbOAk7QRUEh57PPbQ',
-  authDomain: 'devter-6661a.firebaseapp.com',
-  databaseURL: 'https://devter-6661a.firebaseio.com',
-  projectId: 'devter-6661a',
-  storageBucket: 'devter-6661a.appspot.com',
-  messagingSenderId: '807038871343',
-  appId: '1:807038871343:web:cb90ded09f7def7d8063da',
-  measurementId: 'G-909SM8WT3B'
+  apiKey: "AIzaSyC70LAhPV1qHunyNoZbOAk7QRUEh57PPbQ",
+  authDomain: "devter-6661a.firebaseapp.com",
+  databaseURL: "https://devter-6661a.firebaseio.com",
+  projectId: "devter-6661a",
+  storageBucket: "devter-6661a.appspot.com",
+  messagingSenderId: "807038871343",
+  appId: "1:807038871343:web:cb90ded09f7def7d8063da",
+  measurementId: "G-909SM8WT3B",
 }
 
-!firebase.apps.length &&
-  firebase.initializeApp(firebaseConfig)
+!firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
 const mapUserFromFirebaseAuthToUser = (user) => {
   const { displayName, email, photoURL } = user
@@ -20,22 +19,18 @@ const mapUserFromFirebaseAuthToUser = (user) => {
   return {
     avatar: photoURL,
     username: displayName,
-    email
+    email,
   }
 }
 
 export const onAuthStateChanged = (onChange) => {
-  return firebase
-    .auth()
-    .onAuthStateChanged(user => {
-      const normalizedUser = mapUserFromFirebaseAuthToUser(user)
-      onChange(normalizedUser)
-    })
+  return firebase.auth().onAuthStateChanged((user) => {
+    const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+    onChange(normalizedUser)
+  })
 }
 
 export const loginWithGitHub = () => {
   const githubProvider = new firebase.auth.GithubAuthProvider()
-  return firebase
-    .auth()
-    .signInWithPopup(githubProvider)
+  return firebase.auth().signInWithPopup(githubProvider)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import AppLayout from '../components/AppLayout'
-import { colors } from '../styles/theme'
-import Button from '../components/Button'
-import GitHub from '../components/Icons/GitHub'
+import { useEffect, useState } from "react"
+import Head from "next/head"
+import AppLayout from "../components/AppLayout"
+import { colors } from "../styles/theme"
+import Button from "../components/Button"
+import GitHub from "../components/Icons/GitHub"
 
-import {
-  loginWithGitHub,
-  onAuthStateChanged
-} from '../firebase/client'
+import { loginWithGitHub, onAuthStateChanged } from "../firebase/client"
 
-export default function Home () {
+export default function Home() {
   const [user, setUser] = useState(undefined)
 
   useEffect(() => {
@@ -18,40 +15,44 @@ export default function Home () {
   }, [])
 
   const handleClick = () => {
-    loginWithGitHub().then(setUser).catch(err => {
-      console.log(err)
-    })
+    loginWithGitHub()
+      .then(setUser)
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
     <>
       <Head>
-        <title>devter 🐦</title>
+        <title>Capella</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <AppLayout>
         <section>
-          <img src='/devter-logo.png' alt='Logo' />
+          <img src="/devter-logo.png" alt="Logo" />
           <h1>Devter</h1>
-          <h2>Talk about development<br />with developers 👩‍💻👨‍💻</h2>
+          <h2>
+            Talk about development
+            <br />
+            with developers 👩‍💻👨‍💻
+          </h2>
 
           <div>
-            {
-              user === null &&
-                <Button onClick={handleClick}>
-                  <GitHub fill='#fff' width={24} height={24} />
-                  Login with GitHub
-                </Button>
-            }
-            {
-              user && user.avatar && <div>
+            {user === null && (
+              <Button onClick={handleClick}>
+                <GitHub fill="#fff" width={24} height={24} />
+                Login with GitHub
+              </Button>
+            )}
+            {user && user.avatar && (
+              <div>
                 <img src={user.avatar} />
                 <strong>{user.username}</strong>
               </div>
-            }
+            )}
           </div>
-
         </section>
       </AppLayout>
 
